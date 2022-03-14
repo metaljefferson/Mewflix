e422e4d080d8fb3d4cd864aa59ad77ee42a46bea
metaljefferson <metaljefferson11@hotmail.com>
2022-03-14 16:44:14 -0300

    Javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value;
+    if (filmeFavorito.endsWith(".jpg")) {
+        listaFilmesNaTela(filmeFavorito);
+
+    } else {
+        console.error("Endereço de filme invalido");
+    }
+    document.getElementById("filme").value = "";
+}
+
+function listaFilmesNaTela(filme) {
+    var elementoFimeFavorito = "<img src=" + filme + ">";
+    var elementoListaFilmes = document.getElementById("listaFilmes");
+    elementoListaFilmes.innerHTML = elementoListaFilmes.innerHTML + elementoFimeFavorito;
+}
